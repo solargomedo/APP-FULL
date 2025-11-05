6cdcbd3699890d12844db1a76d4fbc1b940a069a
Cristian Arturo Pizarro Muñoz <cri.pizarrom@duocuc.cl>
2025-11-04 23:31:11 -0300

    se Agrega el AuthViewModel para poder guardar el gmail

--- app/src/main/java/com/example/levelupapp/navigation/NavGraph.kt
+++ app/src/main/java/com/example/levelupapp/navigation/NavGraph.kt
@@ -1,18 +1,24 @@
 package com.example.levelupapp.navigation
 
 import androidx.compose.runtime.Composable
+import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.NavHostController
 import com.example.levelupapp.ui.splash.SplashScreen
 import com.example.levelupapp.ui.login.LoginScreen
 import com.example.levelupapp.ui.home.HomeScreen
+import com.example.levelupapp.ui.viewmodel.AuthViewModel
 
 @Composable
+
 fun AppNavGraph(navController: NavHostController) {
+
+    val authViewModel: AuthViewModel = viewModel()
+
     NavHost(navController = navController, startDestination = "splash") {
         composable("splash") { SplashScreen(navController) }
-        composable("login") { LoginScreen(navController) }
-        composable("home") { HomeScreen() }
+        composable("login")   { LoginScreen(navController, authViewModel) }
+        composable("home")    { HomeScreen(authViewModel) }
     }
 }
